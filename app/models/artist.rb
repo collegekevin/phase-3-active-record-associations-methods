cb0754fd79c3011d9ad66f25ff9dbd60c8730692
Kevin Price <collegekevin@gmail.com>
2022-12-01 08:58:36 -0800

    commit tree bd47c39cb26ebc0d6964f545a2d380d0aaf150e5

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,19 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
+    #artist.songs.first.genre - I also thought this would work if
+    #you passed in artist, but seems like it doesn't
+    self.songs.first.genre
     #return the genre of the artist's first saved song
   end
 
   def song_count
+    self.songs.count
     #return the number of songs associated with the artist
   end
 
   def genre_count
+    self.genres.count
     #return the number of genres associated with the artist
   end
 end
